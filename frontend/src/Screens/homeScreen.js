98a31a60b331106fd6a810ad702f14031a6c1392
Asq Arrsl <asqarrsl@gmail.com>
2021-06-23 21:44:12 +0530

    ReInitialize Frontend

--- frontend/src/Screens/homeScreen.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Header from "../Components/header.js";
-
-function HomeScreen() {
-  return <div></div>;
-}
-
-export default HomeScreen;
--- frontend/src/Screens/papers.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Header from "./Components/header.js";
-
-function papersScreen() {
-  return <div>View Papers</div>;
-}
-
-export default papersScreen;
