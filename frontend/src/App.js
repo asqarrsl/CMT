f08e1a44993c8fdd12b5031bb00633ca0f45d3a4
Asq Arrsl <asqarrsl@gmail.com>
2021-06-26 23:27:41 +0530

    Admin Dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import LandingPage from './Views/LandingPage/LangingPage';
-import NavBar from './Components/NavBar/Navbar'
-import Footer from './Components/Footer/Footer'
+import LandingPage from './User/Views/LandingPage/LangingPage';
+import NavBar from './User/Components/NavBar/Navbar'
+import Footer from './User/Components/Footer/Footer'
 import './App.css'
-import Payment from './Views/Payment/Payment';
+import Payment from './User/Views/Payment/Payment';
+import Dashboard from './Admin/Views/Dashboard/Dashboard';
 
 const App = (props) =>{
     return(
@@ -12,8 +13,9 @@ const App = (props) =>{
             <NavBar />
             <Router>
                 <Switch>
-                    <Route path="/" component={LandingPage}  />
                     <Route path="/payment" component={Payment} />
+                    <Route path="/admin/dashboard" component={Dashboard} />
+                    <Route path="/" component={LandingPage}  />
                     {/* <Route path="/" component={LandingPage}  exact/>    */}
                 </Switch>
             </Router>
