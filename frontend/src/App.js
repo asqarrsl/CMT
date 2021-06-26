afe60c566c40049a41e47826c8b2b8812803e6a6
Asq Arrsl <asqarrsl@gmail.com>
2021-06-26 23:55:01 +0530

    Admin Dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,15 +6,19 @@ import Footer from './User/Components/Footer/Footer'
 import './App.css'
 import Payment from './User/Views/Payment/Payment';
 import Dashboard from './Admin/Views/Dashboard/Dashboard';
-
+import AdminRoute from './Admin/AdminRoute'
 const App = (props) =>{
     return(
         <>
             <NavBar />
             <Router>
                 <Switch>
-                    <Route path="/payment" component={Payment} />
                     <Route path="/admin/dashboard" component={Dashboard} />
+
+                    {/* <AdminRoute path="/admin/dashboard" component={Dashboard}></AdminRoute> */}
+                    {/* <AdminRoute path="/user/:id/edit" component={Dashboard}></AdminRoute> */}
+                    
+                    <Route path="/payment" component={Payment} />
                     <Route path="/" component={LandingPage}  />
                     {/* <Route path="/" component={LandingPage}  exact/>    */}
                 </Switch>
