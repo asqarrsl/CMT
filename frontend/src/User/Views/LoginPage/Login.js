5e32c5f02c62c8152a6a5a3f4f3f0ebee71aefe2
vibudathisuraka <vibudathisuraka@gmail.com>
2021-06-27 19:48:50 +0530

    init

--- frontend/src/User/Views/LoginPage/Login.js
+++ frontend/src/User/Views/LoginPage/Login.js
@@ -1,62 +1,48 @@
 import React from "react";
-import "./Login.css";
+import "../../../App.css";
+import SuccessButton from "../../Components/Button/SuccessButton";
+
+const check = () => {
+  console.log("hi");
+};
 
 const Login = () => {
   return (
-    <div>
-      <div class="container">
-        <div class="d-flex justify-content-center h-100">
-          <div class="card">
-            <div class="card-header">
-              <h3>Sign In</h3>
+    <div class="rootBody">
+      <div class="customCardOne">
+        <div class="card-header">
+          <center>
+            <h3>Sign In</h3>
+          </center>
+        </div>
+        <div class="card-body">
+          <form>
+            <div class="form-group">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Username"
+                style={{ border: "1", borderColor: "#1286C1" }}
+              />
             </div>
-            <div class="card-body">
-              <form>
-                <div class="input-group form-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="fas fa-user"></i>
-                    </span>
-                  </div>
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="username"
-                  />
-                </div>
-                <div class="input-group form-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="fas fa-key"></i>
-                    </span>
-                  </div>
-                  <input
-                    type="password"
-                    class="form-control"
-                    placeholder="password"
-                  />
-                </div>
-                <div class="row align-items-center remember">
-                  <input type="checkbox" />
-                  Remember Me
-                </div>
-                <div class="form-group">
-                  <input
-                    type="submit"
-                    value="Login"
-                    class="btn float-right login_btn"
-                  />
-                </div>
-              </form>
+            <div class="form-group">
+              <input
+                type="password"
+                class="form-control"
+                placeholder="Password"
+                style={{ border: "1", borderColor: "#1286C1" }}
+              />
             </div>
-            <div class="card-footer">
-              <div class="d-flex justify-content-center links">
-                Don't have an account?<a href="#">Sign Up</a>
-              </div>
-              <div class="d-flex justify-content-center">
-                <a href="#">Forgot your password?</a>
-              </div>
+            <div class="form-group">
+              <SuccessButton text="Success" onClick={check} position="center" />
             </div>
+          </form>
+        </div>
+        <div class="card-footer">
+          <div class="form-group">
+            <a href="#" style={{ color: "#1286C1", fontSize: "14px" }}>
+              Don't have an account? Sign Up
+            </a>
           </div>
         </div>
       </div>
--- frontend/src/User/Views/RegisterPage/Register.js
+++ frontend/src/User/Views/RegisterPage/Register.js
@@ -1,203 +1,107 @@
 import React from "react";
-import "./Register.css";
-import { Stepper } from "react-form-stepper";
-
-function getSteps() {
-  return ["Select campaign settings", "Create an ad group", "Create an ad"];
-}
-
-function getStepContent(step) {
-  switch (step) {
-    case 0:
-      return "Account details";
-    case 1:
-      return "User details";
-    case 2:
-      return "Confirmation";
-    default:
-      return "Unknown step";
-  }
-}
+import "../../../App.css";
+import SuccessButton from "../../Components/Button/SuccessButton";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bs-stepper/dist/css/bs-stepper.min.css";
+import Stepper from "bs-stepper";
 
 const Register = () => {
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set());
-  const steps = getSteps();
-
-  const isStepOptional = (step) => {
-    return step === 1;
+  const check = () => {
+    console.log("hi");
   };
 
-  const isStepSkipped = (step) => {
-    return skipped.has(step);
-  };
-
-  const handleNext = () => {
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values());
-      newSkipped.delete(activeStep);
-    }
-
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped(newSkipped);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleSkip = () => {
-    if (!isStepOptional(activeStep)) {
-      // You probably want to guard against something like this,
-      // it should never occur unless someone's actively trying to break something.
-      throw new Error("You can't skip a step that isn't optional.");
-    }
-
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped((prevSkipped) => {
-      const newSkipped = new Set(prevSkipped.values());
-      newSkipped.add(activeStep);
-      return newSkipped;
-    });
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
+  const AccountData = () => {
+    return <div></div>;
   };
 
   return (
-    <div>
-      <div class="container">
-        <div class="d-flex justify-content-center h-100">
-          <div class="card">
-            <div class="card-header">
-              <h3> Register </h3>
+    <div class="rootBody">
+      <div class="customCardOne">
+        <div class="card-header">
+          <center>
+            <h3> Register </h3>
+          </center>
+        </div>
+        <div class="card-body">
+          <form>
+            <div class="form-group">
+              <input
+                type="text"
+                id="name"
+                class="form-control"
+                placeholder="Name"
+              />
             </div>
-            <div class="card-body">
-              <form>
-                <div class="input-group form-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="fas fa-user"></i>
-                    </span>
-                  </div>
-                  <input
-                    type="email"
-                    id="email"
-                    class="form-control"
-                    placeholder="email"
-                  />
-                </div>
-
-                <div class="input-group form-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="fas fa-key"></i>
-                    </span>
-                  </div>
-                  <input
-                    type="password"
-                    id="password"
-                    class="form-control"
-                    placeholder="password"
-                  />
-                </div>
-
-                <div class="input-group form-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="fas fa-user"></i>
-                    </span>
-                  </div>
-                  <input
-                    type="text"
-                    id="name"
-                    class="form-control"
-                    placeholder="Name"
-                  />
-                </div>
-
-                <div class="input-group form-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="fas fa-user"></i>
-                    </span>
-                  </div>
-                  <input
-                    type="mobile"
-                    id="text"
-                    class="form-control"
-                    placeholder="Mobile"
-                  />
-                </div>
-
-                <div class="input-group form-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="fas fa-user"></i>
-                    </span>
-                  </div>
-                  <input
-                    type="text"
-                    id="Role"
-                    class="form-control"
-                    placeholder="Role"
-                  />
-                </div>
-
-                <div class="input-group form-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="fas fa-user"></i>
-                    </span>
-                  </div>
-                  <input
-                    type="text"
-                    id="qualification"
-                    class="form-control"
-                    placeholder="Qualification"
-                  />
-                </div>
-
-                <div class="input-group form-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="fas fa-user"></i>
-                    </span>
-                  </div>
-                  <input
-                    type="text"
-                    id="designation"
-                    class="form-control"
-                    placeholder="Designation"
-                  />
-                </div>
-
-                <div class="input-group form-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="fas fa-user"></i>
-                    </span>
-                  </div>
-                  <input
-                    type="text"
-                    id="affiliation"
-                    class="form-control"
-                    placeholder="Affiliation"
-                  />
-                </div>
-
-                <div class="form-group">
-                  <input
-                    type="submit"
-                    value="Login"
-                    class="btn float-right login_btn"
-                  />
-                </div>
-              </form>
+            <div class="form-group">
+              <input
+                type="email"
+                id="email"
+                class="form-control"
+                placeholder="Email"
+              />
             </div>
-            <div class="card-footer"></div>
+
+            <div class="form-group">
+              <input
+                type="password"
+                id="password"
+                class="form-control"
+                placeholder="Password"
+              />
+            </div>
+
+            <div class="form-group">
+              <input
+                type="mobile"
+                id="text"
+                class="form-control"
+                placeholder="Mobile"
+              />
+            </div>
+
+            <select class="form-dropdown" aria-label="Default select example">
+              <option selected>Choose account type</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+
+            <div class="form-group">
+              <input
+                type="text"
+                id="qualification"
+                class="form-control"
+                placeholder="Qualification"
+              />
+            </div>
+
+            <div class="form-group">
+              <input
+                type="text"
+                id="designation"
+                class="form-control"
+                placeholder="Designation"
+              />
+            </div>
+
+            <div class="form-group">
+              <input
+                type="text"
+                id="affiliation"
+                class="form-control"
+                placeholder="Affiliation"
+              />
+            </div>
+            <div class="form-group">
+              <SuccessButton text="Success" onClick={check} position="center" />
+            </div>
+          </form>
+        </div>
+        <div class="card-footer">
+          <div class="form-group">
+            <a href="#" style={{ color: "#1286C1", fontSize: "14px" }}>
+              Don't have an account? Sign Up
+            </a>
           </div>
         </div>
       </div>
