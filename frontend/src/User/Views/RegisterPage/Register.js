9630b0089dc45398187cf6b87346893d886df2b1
vibudathisuraka <vibudathisuraka@gmail.com>
2021-06-29 03:21:32 +0530

    edits

--- frontend/src/User/Views/RegisterPage/Register.js
+++ frontend/src/User/Views/RegisterPage/Register.js
@@ -23,85 +23,108 @@ const Register = () => {
   };
 
   const AccountData = (
-    <div>
-      <div id="stepper1" class="bs-stepper">
-        <div class="bs-stepper-header">
-          {step == 0 ? (
-            <Step title="Account Data" dis="false" />
-          ) : (
-            <Step title="Account Data" dis="true" />
-          )}
-          <div class="line"></div>
-          {step == 1 ? (
-            <Step title="User Data" dis="false" />
-          ) : (
-            <Step title="User Data" dis="true" />
-          )}
-          <div class="line"></div>
-          {step == 2 ? (
-            <Step title="Confirmation" dis="false" />
-          ) : (
-            <Step title="Confirmation" dis="true" />
-          )}
-        </div>
+    <div class="customCardOne" style={{ marginLeft: "70px" }}>
+      <div class="card-header">
+        <center>
+          <h3> Register </h3>
+        </center>
       </div>
-      <div class="customCardOne" style={{ marginLeft: "70px" }}>
-        <div class="card-header">
-          <center>
-            <h3> Register </h3>
-          </center>
+      <div class="card-body"></div>
+      <form>
+        <div class="form-group">
+          <input
+            type="text"
+            id="name"
+            class="form-control"
+            placeholder="Name"
+            required
+          />
         </div>
-        <div class="card-body"></div>
+        <div class="form-group">
+          <input
+            type="email"
+            id="email"
+            class="form-control"
+            placeholder="Email"
+            required
+          />
+        </div>
+
+        <div class="form-group">
+          <input
+            type="password"
+            id="password"
+            class="form-control"
+            placeholder="Password"
+            required
+          />
+        </div>
+
+        <div class="form-group">
+          <input
+            type="mobile"
+            id="text"
+            class="form-control"
+            placeholder="Mobile"
+            required
+          />
+        </div>
+
+        <select
+          class="form-dropdown"
+          aria-label="Default select example"
+          required
+        >
+          <option selected>Choose account type</option>
+          <option value="Researcher">Researcher</option>
+          <option value="Workshop Conductor">WorkshopConductor</option>
+          <option value="Participant">Participant</option>
+        </select>
+        <div class="form-group">
+          <SuccessButton text="Next" onClick={submit} position="center" />
+        </div>
+      </form>
+      <div class="card-footer"></div>
+    </div>
+  );
+  const UserData = (
+    <div class="customCardOne" style={{ marginLeft: "70px" }}>
+      <div class="card-header">
+        <center>
+          <h3> Register </h3>
+        </center>
+      </div>
+      <div class="card-body">
         <form>
           <div class="form-group">
             <input
               type="text"
-              id="name"
+              id="qualification"
               class="form-control"
-              placeholder="Name"
-              required
-            />
-          </div>
-          <div class="form-group">
-            <input
-              type="email"
-              id="email"
-              class="form-control"
-              placeholder="Email"
+              placeholder="Qualification"
               required
             />
           </div>
 
           <div class="form-group">
             <input
-              type="password"
-              id="password"
+              type="text"
+              id="designation"
               class="form-control"
-              placeholder="Password"
+              placeholder="Designation"
               required
             />
           </div>
 
           <div class="form-group">
             <input
-              type="mobile"
-              id="text"
+              type="text"
+              id="affiliation"
               class="form-control"
-              placeholder="Mobile"
+              placeholder="Affiliation"
               required
             />
           </div>
-
-          <select
-            class="form-dropdown"
-            aria-label="Default select example"
-            required
-          >
-            <option selected>Choose account type</option>
-            <option value="Researcher">Researcher</option>
-            <option value="Workshop Conductor">WorkshopConductor</option>
-            <option value="Participant">Participant</option>
-          </select>
           <div class="form-group">
             <SuccessButton text="Next" onClick={submit} position="center" />
           </div>
@@ -110,213 +133,110 @@ const Register = () => {
       </div>
     </div>
   );
-  const UserData = (
-    <div>
-      <div id="stepper1" class="bs-stepper">
-        <div class="bs-stepper-header">
-          <div class="step">
-            <button class="step-trigger" disabled>
-              <span class="bs-stepper-circle" />
-              <span class="bs-stepper-label">Account Data</span>
-            </button>
-          </div>
-          <div class="line"></div>
-          <div class="step">
-            <button class="step-trigger" styles={{ color: "blue" }}>
-              <span class="bs-stepper-circle" />
-              <span class="bs-stepper-label" styles={{ color: "blue" }}>
-                User Data
-              </span>
-            </button>
-          </div>
-          <div class="line"></div>
-          <div class="step">
-            <button class="step-trigger" disabled>
-              <span class="bs-stepper-circle" />
-              <span class="bs-stepper-label">Confirmation</span>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div class="customCardOne" style={{ marginLeft: "70px" }}>
-        <div class="card-header">
-          <center>
-            <h3> Register </h3>
-          </center>
-        </div>
-        <div class="card-body">
-          <form>
-            <div class="form-group">
-              <input
-                type="text"
-                id="qualification"
-                class="form-control"
-                placeholder="Qualification"
-                required
-              />
-            </div>
-
-            <div class="form-group">
-              <input
-                type="text"
-                id="designation"
-                class="form-control"
-                placeholder="Designation"
-                required
-              />
-            </div>
-
-            <div class="form-group">
-              <input
-                type="text"
-                id="affiliation"
-                class="form-control"
-                placeholder="Affiliation"
-                required
-              />
-            </div>
-            <div class="form-group">
-              <SuccessButton text="Next" onClick={submit} position="center" />
-            </div>
-          </form>
-          <div class="card-footer"></div>
-        </div>
-      </div>
-    </div>
-  );
 
   const ConfirmData = (
-    <div>
-      <div id="stepper1" class="bs-stepper">
-        <div class="bs-stepper-header">
-          <div class="step">
-            <button class="step-trigger" disabled>
-              <span class="bs-stepper-circle" />
-              <span class="bs-stepper-label">Account Data</span>
-            </button>
-          </div>
-          <div class="line"></div>
-          <div class="step">
-            <button class="step-trigger" disabled>
-              <span class="bs-stepper-circle" />
-              <span class="bs-stepper-label">User Data</span>
-            </button>
-          </div>
-          <div class="line"></div>
-          <div class="step">
-            <button class="step-trigger" styles={{ color: "blue" }}>
-              <span class="bs-stepper-circle" />
-              <span class="bs-stepper-label" styles={{ color: "blue" }}>
-                Confirmation
-              </span>
-            </button>
+    <div style={{ width: "100%", display: "table" }}>
+      <div style={{ display: "table-row" }}>
+        <div style={{ width: "600px", display: "table-cell" }}>
+          <div class="customCardOne" style={{ marginLeft: "70px" }}>
+            <div class="card-header">
+              <center>
+                <h3> Register </h3>
+              </center>
+            </div>
+            <div class="card-body"></div>
+            <form>
+              <div class="form-group">
+                <input
+                  type="text"
+                  id="name"
+                  class="form-control"
+                  placeholder="Name"
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  type="email"
+                  id="email"
+                  class="form-control"
+                  placeholder="Email"
+                />
+              </div>
+
+              <div class="form-group">
+                <input
+                  type="password"
+                  id="password"
+                  class="form-control"
+                  placeholder="Password"
+                />
+              </div>
+
+              <div class="form-group">
+                <input
+                  type="text"
+                  id="mobile"
+                  class="form-control"
+                  placeholder="Mobile"
+                />
+              </div>
+
+              <div class="form-group">
+                <input
+                  type="text"
+                  id="accountType"
+                  class="form-control"
+                  placeholder="Account Type"
+                />
+              </div>
+            </form>
+            <div class="card-footer"></div>
           </div>
         </div>
-      </div>
-
-      <div style={{ width: "100%", display: "table" }}>
-        <div style={{ display: "table-row" }}>
-          <div style={{ width: "600px", display: "table-cell" }}>
-            <div class="customCardOne" style={{ marginLeft: "70px" }}>
-              <div class="card-header">
-                <center>
-                  <h3> Register </h3>
-                </center>
-              </div>
-              <div class="card-body"></div>
+        <div style={{ display: "table-cell" }}>
+          <div class="customCardOne" style={{ marginLeft: "70px" }}>
+            <div class="card-header">
+              <center>
+                <h3> Register </h3>
+              </center>
+            </div>
+            <div class="card-body">
               <form>
                 <div class="form-group">
                   <input
                     type="text"
-                    id="name"
+                    id="qualification"
                     class="form-control"
-                    placeholder="Name"
-                  />
-                </div>
-                <div class="form-group">
-                  <input
-                    type="email"
-                    id="email"
-                    class="form-control"
-                    placeholder="Email"
-                  />
-                </div>
-
-                <div class="form-group">
-                  <input
-                    type="password"
-                    id="password"
-                    class="form-control"
-                    placeholder="Password"
+                    placeholder="Qualification"
                   />
                 </div>
 
                 <div class="form-group">
                   <input
                     type="text"
-                    id="mobile"
+                    id="designation"
                     class="form-control"
-                    placeholder="Mobile"
+                    placeholder="Designation"
                   />
                 </div>
 
                 <div class="form-group">
                   <input
                     type="text"
-                    id="accountType"
+                    id="affiliation"
                     class="form-control"
-                    placeholder="Account Type"
+                    placeholder="Affiliation"
                   />
                 </div>
               </form>
               <div class="card-footer"></div>
             </div>
           </div>
-          <div style={{ display: "table-cell" }}>
-            <div class="customCardOne" style={{ marginLeft: "70px" }}>
-              <div class="card-header">
-                <center>
-                  <h3> Register </h3>
-                </center>
-              </div>
-              <div class="card-body">
-                <form>
-                  <div class="form-group">
-                    <input
-                      type="text"
-                      id="qualification"
-                      class="form-control"
-                      placeholder="Qualification"
-                    />
-                  </div>
 
-                  <div class="form-group">
-                    <input
-                      type="text"
-                      id="designation"
-                      class="form-control"
-                      placeholder="Designation"
-                    />
-                  </div>
+          <div class="form-group" style={{ paddingLeft: "75px" }}>
+            <SuccessButton text="Reset" onClick={reset} position="center" />
 
-                  <div class="form-group">
-                    <input
-                      type="text"
-                      id="affiliation"
-                      class="form-control"
-                      placeholder="Affiliation"
-                    />
-                  </div>
-                </form>
-                <div class="card-footer"></div>
-              </div>
-            </div>
-
-            <div class="form-group" style={{ paddingLeft: "75px" }}>
-              <SuccessButton text="Reset" onClick={reset} position="center" />
-
-              <SuccessButton text="Submit" onClick={done} position="center" />
-            </div>
+            <SuccessButton text="Submit" onClick={done} position="center" />
           </div>
         </div>
       </div>
@@ -324,13 +244,36 @@ const Register = () => {
   );
   return (
     <div class="rootBody">
-      {step === 0
-        ? AccountData
-        : step === 1
-        ? UserData
-        : step === 2
-        ? ConfirmData
-        : null}
+      <div>
+        <div id="stepper1" class="bs-stepper" style={{ maxWidth: "250px" }}>
+          <div class="bs-stepper-header">
+            {step == 0 ? (
+              <Step title="Account Data" dis="false" />
+            ) : (
+              <Step title="Account Data" dis="true" />
+            )}
+            <div class="line"></div>
+            {step == 1 ? (
+              <Step title="User Data" dis="false" />
+            ) : (
+              <Step title="User Data" dis="true" />
+            )}
+            <div class="line"></div>
+            {step == 2 ? (
+              <Step title="Confirmation" dis="false" />
+            ) : (
+              <Step title="Confirmation" dis="true" />
+            )}
+          </div>
+        </div>
+        {step === 0
+          ? AccountData
+          : step === 1
+          ? UserData
+          : step === 2
+          ? ConfirmData
+          : null}
+      </div>
     </div>
   );
 };
