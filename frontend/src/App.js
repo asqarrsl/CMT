49243b859f2f8a8edf37be3a787d20447fa5feb1
vibudathisuraka <vibudathisuraka@gmail.com>
2021-06-29 15:19:01 +0530

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,32 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import LandingPage from './User/Views/LandingPage/LangingPage';
-import NavBar from './User/Components/NavBar/Navbar'
-import Footer from './User/Components/Footer/Footer'
-import './App.css'
-import Payment from './User/Views/Payment/Payment';
-import Dashboard from './Admin/Views/Dashboard/Dashboard';
-import AdminRoute from './Admin/AdminRoute';
-import MainAdmin from './Admin/Components/Layout/MainAdmin';
-import Main from './User/Components/Layout/Main';
-import UserIndex from './Admin/Views/User';
-import AddUser from './Admin/Views/User/Add';
-import EditUser from './Admin/Views/User/Edit';
-import AddEvent from './Admin/Views/Event/Add';
-import EditEvent from './Admin/Views/Event/Edit';
-import EventIndex from './Admin/Views/Event';
-import EditMaterial from './Admin/Views/Material/Edit';
-import AddMaterial from './Admin/Views/Material/Add';
-import MaterialIndex from './Admin/Views/Material';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LandingPage from "./User/Views/LandingPage/LangingPage";
+import NavBar from "./User/Components/NavBar/Navbar";
+import Footer from "./User/Components/Footer/Footer";
+import "./App.css";
+import Payment from "./User/Views/Payment/Payment";
+import Dashboard from "./Admin/Views/Dashboard/Dashboard";
+import AdminRoute from "./Admin/AdminRoute";
+import MainAdmin from "./Admin/Components/Layout/MainAdmin";
+import Main from "./User/Components/Layout/Main";
+import UserIndex from "./Admin/Views/User";
+import AddUser from "./Admin/Views/User/Add";
+import EditUser from "./Admin/Views/User/Edit";
+import AddEvent from "./Admin/Views/Event/Add";
+import EditEvent from "./Admin/Views/Event/Edit";
+import EventIndex from "./Admin/Views/Event";
+import EditMaterial from "./Admin/Views/Material/Edit";
+import AddMaterial from "./Admin/Views/Material/Add";
+import MaterialIndex from "./Admin/Views/Material";
 import Login from "./User/Views/LoginPage/Login";
 import Register from "./User/Views/RegisterPage/Register";
 import UserProfile from "./User/Views/UserProfilePage/UserProfile";
 import WorkshopMgt from "./User/Views/WorkshopMgtPage/WorkshopMgt";
 
 const App = (props) => {
+  const [userInfo, setUserInfo] = useState();
+  const [token, setToken] = useState();
+
   return (
     <>
       <Router>
