e48c3e7d4e23e5ccce1f1befe1a6b0323cf4ba2b
vibudathisuraka <vibudathisuraka@gmail.com>
2021-06-29 06:46:49 +0530

    merge from dev1

--- frontend/src/Admin/Views/Material/MaterialDataTable.js
+++ frontend/src/Admin/Views/Material/MaterialDataTable.js
@@ -1,27 +1,36 @@
-import React from 'react'
+import React from "react";
 
-const MaterialDataTable = ({data}) =>{
-    var editUrl = (obj)=>{
-        return `/admin/material/${obj}/edit`
-    }
-    var deleteUrl = (obj)=>{
-        return `/admin/material/${obj}/edit`
-    }
-    return(
-        <tbody>
-            {data.map((material,i)=>(
-                <tr key={i}> 
-                    <th scope="row">{i}</th>
-                    <td>{material.name}</td>
-                    <td>{material.type}</td>
-                    <td>{material.eventId.eventName}</td>
-                    <td>{material.uid.name}</td>
-                    <td><a href={editUrl(material._id)}><i className="fas fa-pen"></i></a></td>
-                    <td><a href={deleteUrl(material._id)}><i className="far fa-trash-alt"></i></a></td>
-                </tr>
-            ))}
-        </tbody>
-    );
-}
+const MaterialDataTable = ({ data }) => {
+  var editUrl = (obj) => {
+    return `/admin/material/${obj}/edit`;
+  };
+  var deleteUrl = (obj) => {
+    return `/admin/material/${obj}/edit`;
+  };
+  return (
+    <tbody>
+      {data.map((materials, i) => (
+        <tr key={i}>
+          <th scope="row">{i}</th>
+          <td>{materials.name}</td>
+          <td>{materials.type}</td>
+          <td>{materials.eventId.eventName}</td>
+          <td>{materials.uid.name}</td>
+          {console.log(materials)}
+          <td>
+            <a href={editUrl(materials._id)}>
+              <i className="fas fa-pen"></i>
+            </a>
+          </td>
+          <td>
+            <a href={deleteUrl(materials._id)}>
+              <i className="far fa-trash-alt"></i>
+            </a>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  );
+};
 
 export default MaterialDataTable;
