034db5188daead18a4fdff02edd274565b19c8fc
Asq Arrsl <asqarrsl@gmail.com>
2021-06-30 11:31:30 +0530

    Dashboard

--- frontend/src/Admin/Components/Header/Header.js
+++ frontend/src/Admin/Components/Header/Header.js
@@ -1,44 +1,138 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { removeUserSession, getToken } from "../../../Utils/Common";
 
 const Header = () => {
-    return(
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a className="dropdown-item" href="#">Action</a></li>
-                            <li><a className="dropdown-item" href="#">Another action</a></li>
-                            <li><hr className="dropdown-divider" /></li>
-                            <li><a className="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                        </li>
-                        <li className="nav-item">
-                        {/* <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a> */}
-                        </li>
-                    </ul>
-                    <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
+  const [loggedin, setloggedin] = useState(false);
+  useEffect(() => {
+    const token = getToken();
+    setloggedin(token);
+  }, []);
+  const handleLogout = (e) => {
+    e.preventDefault();
+    axios
+      .get("http://localhost:3000/users/logout")
+      .then((response) => {
+        removeUserSession();
+        setloggedin(false);
+        window.location = `/`;
+        alert("Successfully logged out");
+      })
+      .catch((error) => {
+        if (error.response) {
+          // if (error.response.status === 401) setError(error.response.data.message);
+          // else setFormError("Something went wrong. Please try again later.");
+        } else if (error.request) {
+        } else {
+        }
+      });
+    // window.location = `/`;
+  };
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          CMT Admin Panel
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Events
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a className="dropdown-item" href="/events">
+                    Workshops
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/conference">
+                    conferences
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Papers
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a className="dropdown-item" href="/papers">
+                    Workshop Papers
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/research_papers">
+                    Research Papers
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              {/* <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a> */}
+            </li>
+          </ul>
+          {loggedin ? (
+            <div>
+              <a
+                type="button"
+                class="btn btn-outline-primary"
+                style={{ marginRight: "10px", borderRadius: "30px" }}
+                href="/userProfile"
+              >
+                <i class="fas fa-user-circle"></i>
+              </a>
+              <button
+                className="btn btn-outline-warning"
+                type="submit"
+                onClick={handleLogout}
+              >
+                <i class="fas fa-sign-out-alt" /> Logout
+              </button>
             </div>
-        </nav>
-    );
-}
+          ) : (
+            <div>
+              <a className="btn btn-outline-warning" href="/login">
+                <i class="fas fa-sign-in-alt" /> Login
+              </a>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
